acbf3792b7061c5df594b37263bdb2479a13acd6
PMBPG <pmbpk@yahoo.com>
2024-05-17 11:09:52 +0330

    build category page & filter food

--- components/templates/CategoriesPage.js
+++ components/templates/CategoriesPage.js
@@ -1,14 +1,22 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import styles from './CategoriesPage.module.css'
 import { useRouter } from 'next/router';
+import Card from "../modules/Card"
 
-function CategoriesPage() {
+function CategoriesPage({data}) {
     const router = useRouter();
 
     const [query, setQuery] = useState({
         difficulty: "",
         time: "",
     });
+
+    useEffect(() => {
+        const { difficulty, time } = router.query;
+        if(query.difficulty !== difficulty || query.time !== time){
+            setQuery({difficulty, time})
+        }
+    }, [])
     //-----------------events---------------------
 
     const changeHandler = (e) => {
@@ -38,6 +46,10 @@ function CategoriesPage() {
                     <option value="less">Less than 30 min</option>
                 </select>
                 <button onClick={clickHandler}>Search</button>
+            </div>
+            <div className={styles.cards}>
+                {!data.length && <img src='/images/search.png' alt='Category' />}
+                {data.map( food => (<Card key={food.id} {...food}/>))}
             </div>
         </div>
     </div>
--- pages/categories/index.js
+++ pages/categories/index.js
@@ -1,9 +1,52 @@
 import CategoriesPage from "../../components/templates/CategoriesPage"
 
-function Categories() {
+function Categories({data}) {
   return (
-    <CategoriesPage />
+    <CategoriesPage data={data} />
   )
 }
 
 export default Categories
+
+
+
+//------------------SSR----------------------------------------------
+
+export async function getServerSideProps(context){
+  const { 
+    query: {difficulty, time} 
+  } = context;
+
+  const res = await fetch(`http://localhost:4000/data`);
+  const json = await res.json();
+
+  //---------filer query ------------------------------
+  const data = json.filter((i) => {
+
+
+    const difficultyRes = i.details.filter(
+      (d) => d.Difficulty && d.Difficulty === difficulty)
+      
+    const timeRes = i.details.filter(d => {
+      const CookingTime = d["Cooking Time"] || "";
+      const [timeDetails] = CookingTime.split(" ");
+      if(time === "less" && timeDetails && +timeDetails <= 30){
+        return d;
+      } else if(time === "more" && timeDetails && +timeDetails > 30){
+        return d
+      }
+    });
+
+    if(time && difficulty && timeRes.length && difficultyRes.length){
+      return i
+    } else if(!time && difficulty && difficultyRes.length){
+      return i
+    } else if(!difficulty && time && timeRes.length){
+      return i
+    }
+  })
+
+  return {
+    props:{ data },
+  }
+}
